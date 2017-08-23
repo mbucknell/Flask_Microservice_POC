7fb8c68b8754eb784e99b11a0bc928fe5f6d2395
Mary Bucknell <mbucknell@usgs.gov>
2017-08-23 13:09:01 -0500

    Added a message service and played around with models. Also added the ability to read environment variable from a file, '.env'

---
+++ config.py
@@ -0,0 +1,3 @@
+# This file contains the default values for the application's configuration variables
+# Typically this file will be overridden by a deploy specific configuration file
+DEBUG = False
--- services.py
+++ services.py
@@ -1,24 +1,53 @@
 from flask import request
 from flask_restplus import fields, Api, Resource
+from werkzeug.exceptions import BadRequest
 
 from app import application
 
 api = Api(application)
 
-message_model = api.model('HelloModel', {
+message_model = api.model('MessageModel', {
     'message' : fields.String,
-    'name' : fields.String
+})
+
+hello_model = api.inherit('HelloModel', message_model, {
+    'message' : fields.String('Hello'),
+    'name': fields.String
 })
 
 @api.route('/hello')
 @api.route('/hello/<name>')
 class HelloWorld(Resource):
-    @api.marshal_with(message_model)
+    @api.marshal_with(hello_model)
+    @api.doc(params={'name': 'Name to say hello to. Defaults to World'})
     def get(self, name='World'):
         return {
             'message': 'hello',
             'name': name
         }
+    @api.marshal_with(hello_model)
+    @api.doc(params={'name': 'Name to say hello to'})
+    def post(self, name):
+        if name:
+            return {
+                'message': 'hello',
+                'name' : name
+            }, 201
+        else:
+            raise BadRequest('No name')
 
-    def put(self):
-        return {'message' : 'Not yet implemented'}, 400
+@api.route('/message/<msg>')
+
+class Message(Resource):
+    @api.marshal_with(message_model)
+    def get(self, msg):
+        return {
+            'message': msg
+        }
+    @api.marshal_with(message_model)
+    def post(self, msg):
+        return {
+            'message': msg,
+        }, 201
+
+
