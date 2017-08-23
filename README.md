5ef3431c6489e48e1fc79ad1b3b598b919494e83
Mary Bucknell <mbucknell@usgs.gov>
2017-08-23 14:27:18 -0500

    Updated the README and added requirements.txt. Also added some parameters to make nicer API documentation.

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # Flask_Microservice_POC
+
+This project can be build by creating a virtualenv for python 3 and then installing the required packages listed in
+requirements.txt as follows:
+```
+% virtualenv --python=python3 env
+% env/bin/pip install -r requirements.txt
+```
+
+A local development server can then be run as follows:
+```
+% env/bin/python app.py
+```
+The file config.py contains the default configuration parameters for this project. Deployment specific configuration
+parameters may be specified in a .env file.
+
+The api documentation will appear at '/api'.
--- services.py
+++ services.py
@@ -1,10 +1,16 @@
-from flask import request
+from flask import Blueprint
 from flask_restplus import fields, Api, Resource
 from werkzeug.exceptions import BadRequest
 
 from app import application
 
-api = Api(application)
+
+api = Api(application,
+          title='Flask MLR POC',
+          description='Proof of Concept for Flask Microservices',
+          default='mlr_poc',
+          default_label='MLR POC',
+          doc='/api')
 
 message_model = api.model('MessageModel', {
     'message' : fields.String,
@@ -26,7 +32,8 @@ class HelloWorld(Resource):
             'name': name
         }
     @api.marshal_with(hello_model)
-    @api.doc(params={'name': 'Name to say hello to'})
+    @api.doc(params={'name': 'Name to say hello to'},
+             responses={201: "Created"})
     def post(self, name):
         if name:
             return {
@@ -45,6 +52,7 @@ class Message(Resource):
             'message': msg
         }
     @api.marshal_with(message_model)
+    @api.doc(responses={201: "Created"})
     def post(self, msg):
         return {
             'message': msg,
